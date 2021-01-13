9a57fddce274b664e874ba9cf63f74aa6519f015
Niicksan <34044055+Niicksan@users.noreply.github.com>
2021-01-13 12:46:06 +0200

    Updating Display all post page

--- src/Pages/DisplayAllPosts.js
+++ src/Pages/DisplayAllPosts.js
@@ -1,54 +1,33 @@
 import '../index.css';
-import React from "react";
-import Post from './Post'
-import { Card} from 'react-bootstrap';
-import { posts } from "../Jsons/posts.json";
-import { users } from "../Jsons/users.json";
+import React, { useState } from 'react';
+import Posts from '..Pages/Posts'
+import { jsonPosts } from "../Jsons/posts.json";
 import { Link, Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 export default class DisplayAllPosts extends React.Component {
 	render() {
-		const postsObject = posts;
-		const usersObjects = users;
-		
+		const [posts] = useState(jsonPosts); 
+  		const [currentPage, setCurrentPage] = useState(1);
+  		const [postsPerPage] = useState(10);
+
+  		
+		//Get current posts
+		const indexOfLastPost = currentPage * postsPerPage;
+		const indexOfFirstPost = indexOfLastPost - postsPerPage;
+		const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+		//Change page
+		const paginate = pageNumber => setCurrentPage(pageNumber);
+				
 		return (
 			<>
 				<div class="wrap">
-                	<div class="grid-container">
-						<div class="main-posts" >
-							{postsObject.map((postData) => {
-								return (
-									<Router>
-										<Card className="card" 
-											key={postData.id}
-											post={postsObject.find(post => post.id === postData.id)}
-											>
-
-												<Card.Header>
-													<span className="bold">{postData.title}</span>
-													</Card.Header>
-												<Card.Body>
-													<Card.Text className="">
-														{postData.body}
-													</Card.Text>
-													<Card.Text className="">
-														By: {usersObjects.filter(function(el){return el.id === postData.userId})[0].name}
-													</Card.Text>
-												</Card.Body>
-											
-										</Card>
-										
-										<Route 
-											path='/posts/{postData.id}'
-											render={(props) => <Post {...props} 
-												post={postsObject.find(post => post.id === postData.id)} 
-												author={usersObjects.filter(function(el){return el.id === postData.userId})[0].name} 
-										/>} />
-									</Router>
-									);
-								})
-							}
-						</div>
+					<div class="grid-container">
+						<Posts posts={currentPosts}/>
+						<Pagination 
+							postsPerPage={postsPerPage} 
+							totalPosts={posts.length} 
+							paginate={paginate} />
 					</div>
 				</div>
 			</>
