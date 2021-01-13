2f219010f146415aae74903ebda843172584d17b
Niicksan <34044055+Niicksan@users.noreply.github.com>
2021-01-13 12:53:18 +0200

    Updating Posts page

--- src/Pages/Posts.js
+++ src/Pages/Posts.js
@@ -1,31 +1,33 @@
 import '../index.css';
 import React from "react";
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
+import { jsonUsers } from "../Jsons/users.json";
 
-export default class Post extends React.Component {
-    constructor(props) {
-        super(props);
-        };
-        
-	render() {		
-		return (
-			<>
-			    <div class="main-posts" >
-			    	<Card className="card" >
-                        <Card.Header>
-			    			<span className="bold">{this.props.title}</span>
-			    			</Card.Header>
-			    		<Card.Body>
-			    			<Card.Text className="">
-                                {this.props.body}
-			    			</Card.Text>
-			    			<Card.Text className="">
-			    				By: {this.props.name}
-			    			</Card.Text>
-			    		</Card.Body>
-			    	</Card>
-				</div>
-			</>
-		);
-	}
-}
+const Posts = ({ posts}) => {
+  const [users] = useState(jsonUsers); 
+
+  return (
+    <div class="main-posts" >
+      {posts.map(post => (
+        <Card className="card" key={post.id}>
+          <Card.Header>
+            <span className="bold">
+              {post.title}
+            </span>
+          </Card.Header>
+          <Card.Body>
+			      <Card.Text className="">
+              {post.body}
+			    	</Card.Text>
+			    	<Card.Text className="">
+			    		Posted by: {users.filter(function(u){return u.id === post.userId})[0].name}
+			    	</Card.Text>
+			    </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
