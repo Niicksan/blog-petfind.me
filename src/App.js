688e36aa136e45aa33fd33f0aec9f7a93ae2d028
Niicksan <34044055+Niicksan@users.noreply.github.com>
2021-01-21 16:26:46 +0200

    Updating App.js component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
 import { Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Navbar from './Pages/Navbar' 
 import Footer from './Pages/Footer' 
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
+// import Login from "./Pages/Login";
+// import Register from "./Pages/Register";
 import NotFound from "./Pages/NotFound";
 import DisplayAllUsers from "./Pages/DisplayAllUsers";
 import DisplayAllPosts from "./Pages/DisplayAllPosts";
@@ -21,8 +20,8 @@ function App() {
           <div className="main-users" >
             <Switch>
               <Route path={"/"} exact component={DisplayAllPosts} />
-              <Route path={"/login"} component={Login} />
-              <Route path={"/register"} component={Register} />
+              {/* <Route path={"/login"} component={Login} />
+              <Route path={"/register"} component={Register} /> */}
               <Route path={"/not-found"} component={NotFound} />
               <Route path={"/display-all-users"} component={DisplayAllUsers} />
               <Route path={"/display-all-posts"} component={DisplayAllPosts} />
