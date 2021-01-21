a6c0713f97dceddf7db24ade8ef51b8ae5b3c5de
Niicksan <34044055+Niicksan@users.noreply.github.com>
2021-01-21 16:21:30 +0200

    Updating Display all comments page

--- src/Pages/DisplayAllComments.js
+++ src/Pages/DisplayAllComments.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import CommentsPagination from '../Pages/CommentsPagination'
 import Comments from "./Comments";
 
